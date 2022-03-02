04b78101fd228c71fb19a7359c8a17de7cf2692f
Luiz Satto <lsatto@storm.ie>
2022-03-02 18:28:16 +0000

    Added SimpleBookServiceDbConfig and configured our user secrets

---
+++ SimpleBookService.Core/SimpleBookServiceDbConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleBookService.Core
+{
+    public class SimpleBookServiceDbConfig
+    {
+        public string BOOKS_COLLECTION_NAME { get; set; }
+        public string CONNECTION_STRING { get; set; }
+        public string DATABASE_NAME { get; set; }
+    }
+}
