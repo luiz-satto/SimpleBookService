8aca87c119a4cbaf50da8a7262742351db268678
Luiz Satto <lsatto@storm.ie>
2022-03-02 17:47:25 +0000

    Created new WebApi to deal with the Books (CRUD)

---
+++ SimpleBookService.WebApi/Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleBookService.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+        public BooksController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            return Ok();
+        }
+    }
+}
