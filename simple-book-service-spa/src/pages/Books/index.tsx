48fdbcf31b77a85177d6c59347c1ac979bc864bd
Luiz Satto <luizf.satto@gmail.com>
2022-03-02 22:54:34 -0300

    Started to pull data from api and added CRUD Buttons

--- simple-book-service-spa/src/pages/Books/index.tsx
+++ simple-book-service-spa/src/pages/Books/index.tsx
@@ -1,50 +1,104 @@
 import React, { useState, useEffect } from "react";
-import { Table } from "react-bootstrap";
+import { Badge, Button, Table } from "react-bootstrap";
+
 import api from "../../services/simple-book-service.api";
+import moment from "moment"
+
+interface IBook {
+    id: string;
+    name: string;
+    author: string;
+    category: string;
+    description: string;
+    price: number;
+    registration: Date;
+}
+
+enum BookCategory {
+    Thriller = "thriller",
+    History = "history",
+    Drama = "drama",
+    Biography = "biography"
+}
 
 const Books: React.FC = () => {
+
+    const [books, setBooks] = useState<IBook[]>([])
 
     useEffect(() => {
         getBooks()
     }, [])
 
     async function getBooks() {
-        const response = await api.get("/GetBooks");
-        console.log(response);
+        const response = await api.get("/GetBooks")
+        console.log(response)
+        setBooks(response.data)
+    }
+
+    function formatDate(date: Date) {
+        return moment(date).format('DD/MM/YYYY')
+    }
+
+    function getCategoryBadge(category: string) {
+        let _category = category.toLocaleLowerCase()
+        if (_category === BookCategory.Biography) {
+            return "primary"
+        }
+
+        if (_category === BookCategory.Drama) {
+            return "danger"
+        }
+
+        if (_category === BookCategory.History) {
+            return "info"
+        }
+
+        return "secondary"
     }
 
     return (
         <div className="container">
             <br />
-            <h1>Books Page</h1>
-            <br />
-            <Table striped bordered hover>
+            <h1><Button className="pb-2" size="lg" variant="primary">Create New Book</Button> {' '}</h1>
+
+            <Table className="text-center" striped bordered hover>
                 <thead>
                     <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
+                        <th>Actions</th>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Author</th>
+                        <th>Category</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                        <th>Registration</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
+                    {
+                        books.map(book => {
+                            return (
+                                <tr key={book.id}>
+                                    <td>
+                                        <Button size="sm" variant="secondary">Edit</Button> {' '}
+                                        <Button size="sm" variant="info">View</Button> {' '}
+                                        <Button size="sm" variant="danger">Delete</Button> {' '}
+                                    </td>
+                                    <td>{book.id}</td>
+                                    <td>{book.name}</td>
+                                    <td>{book.author}</td>
+                                    <td>
+                                        <Badge pill bg={getCategoryBadge(book.category)}>
+                                            {book.category}
+                                        </Badge>
+                                    </td>
+                                    <td>{book.description}</td>
+                                    <td>{book.price}</td>
+                                    <td>{formatDate(book.registration)}</td>
+                                </tr>
+                            );
+                        })
+                    }
                 </tbody>
             </Table>
         </div>
